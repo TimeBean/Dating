960b4e79924b273d78a23896d1f4f51e04348f67
Alexander <alexanderklush8@gmail.com>
2026-02-04 15:19:01 +0300

    refactor: remove everything except API

--- DatingTelegramBot/ObjectStores/S3Options.cs
+++
@@ -1,8 +0,0 @@
-namespace DatingTelegramBot.ObjectStores;
-
-public class S3Options {
-    public string ServiceUrl { get; set; }
-    public string AccessKey { get; set; }
-    public string SecretKey { get; set; }
-    public string BucketName { get; set; }
-}
